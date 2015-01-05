10edf5465bdfa5a0838ae715a8d3ba8320d77983
Mike <mthomsonbrown@gmail.com>
2015-01-04 17:47:26 -0800

    Added stub login and register activities

--- app/src/main/java/com/slashandhyphen/tasterly/HomeActivity.java
+++ app/src/main/java/com/slashandhyphen/tasterly/HomeActivity.java
@@ -3,6 +3,8 @@ package com.slashandhyphen.tasterly;
 import com.savagelook.android.*;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,13 +22,14 @@ import java.util.List;
 public class HomeActivity extends ActionBarActivity {
 
     private static final String TASKS_URL = "http://192.168.1.102:3000/api/v1/tasks.json";
+    private SharedPreferences mPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        loadTasksFromAPI(TASKS_URL);
+        mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
     }
 
     private void loadTasksFromAPI(String url) {
@@ -68,24 +71,34 @@ public class HomeActivity extends ActionBarActivity {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mPreferences.contains("AuthToken")) {
+            loadTasksFromAPI(TASKS_URL);
+        } else {
+            Intent intent = new Intent(HomeActivity.this, WelcomeActivity.class);
+            startActivityForResult(intent, 0);
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                loadTasksFromAPI(TASKS_URL);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
