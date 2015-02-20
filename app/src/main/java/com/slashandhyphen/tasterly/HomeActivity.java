0a46fcb34b389375243adc3602e265a6bf73f126
Mike <mthomsonbrown@gmail.com>
2015-02-19 18:48:43 -0800

    Cleanup x2

--- app/src/main/java/com/slashandhyphen/tasterly/HomeActivity.java
+++ app/src/main/java/com/slashandhyphen/tasterly/HomeActivity.java
@@ -1,7 +1,5 @@
 package com.slashandhyphen.tasterly;
 
-import com.savagelook.android.*;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
