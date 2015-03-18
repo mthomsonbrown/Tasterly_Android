19a266359e473b97296c9a8ffea81ad5e57fe875
Mike <mthomsonbrown@gmail.com>
2015-03-17 23:46:14 -0700

    Made test buttons arrays

--- app/src/main/java/com/slashandhyphen/tasterly/AddBeerFragment.java
+++ app/src/main/java/com/slashandhyphen/tasterly/AddBeerFragment.java
@@ -1,23 +1,19 @@
 package com.slashandhyphen.tasterly;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
-import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 
 public class AddBeerFragment extends Fragment {
-    Button testButton;
+    Button testButtons[] = new Button[2];
     RelativeLayout rl;
     FragmentActivity fa;
     String TAG = "AddBeer";
@@ -32,12 +28,15 @@ public class AddBeerFragment extends Fragment {
 
         // TODO Make this not return 0.0
         origin = rl.getHeight();
-
         Toast.makeText(getActivity(), "RL Height is " + origin, Toast.LENGTH_SHORT).show();
-        testButton = (Button) rl.findViewById(R.id.test_button);
-        testButton.setOnClickListener(new mClickListener());
-        testButton.setOnTouchListener(new mTouchListener());
-        testButton.setOnDragListener(new mDragListener());
+        testButtons[0] = (Button) rl.findViewById(R.id.test_button1);
+        testButtons[0].setOnClickListener(new mClickListener());
+        testButtons[0].setOnTouchListener(new mTouchListener());
+        testButtons[1] = (Button) rl.findViewById(R.id.test_button2);
+        testButtons[1].setOnClickListener(new mClickListener());
+        testButtons[1].setOnTouchListener(new mTouchListener());
+
+        rl.setOnTouchListener(new mTouchListener());
 
         return rl;
     }
@@ -51,24 +50,32 @@ public class AddBeerFragment extends Fragment {
     }
 
     class mTouchListener implements View.OnTouchListener {
+        float testOriginX[] = new float[2];
+        float testOriginY[] = new float[2];
+        float touchOriginX, touchOriginY = 0;
+
         @Override
         public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                testOriginX[0] = testButtons[0].getX();
+                testOriginY[0] = testButtons[0].getY();
+                testOriginX[1] = testButtons[1].getX();
+                testOriginY[1] = testButtons[1].getY();
+                touchOriginX = event.getRawX();
+                touchOriginY = event.getRawY();
+            }
             if (event.getAction() == MotionEvent.ACTION_MOVE) {
-
-                // TODO Make not spastic
-                testButton.setY(event.getY());
-                testButton.setX(event.getX());
+                float dY = touchOriginY - event.getRawY();
+                float dX = touchOriginX - event.getRawX();
+                testButtons[0].setY(testOriginY[0] - dY);
+                testButtons[0].setX(testOriginX[0] - dX);
+                testButtons[1].setY(testOriginY[1] - dY);
+                testButtons[1].setX(testOriginX[1] - dX);
 
             }
 
             return false;
         }
     }
-    class mDragListener implements View.OnDragListener {
-        @Override
-        public boolean onDrag(View v, DragEvent event) {
-            Toast.makeText(getActivity(), "Drag Beer", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-    }
+
 }
