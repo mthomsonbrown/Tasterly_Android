b94b97c4d91c3e5de21c4d6acd0570157cb84632
Mike <mthomsonbrown@gmail.com>
2015-03-18 00:34:18 -0700

    Gussied slightly; click events consumed by touchListener

--- app/src/main/java/com/slashandhyphen/tasterly/AddBeerFragment.java
+++ app/src/main/java/com/slashandhyphen/tasterly/AddBeerFragment.java
@@ -13,7 +13,10 @@ import android.widget.Toast;
 
 
 public class AddBeerFragment extends Fragment {
+
+    // TODO Dynamasize
     Button testButtons[] = new Button[2];
+
     RelativeLayout rl;
     FragmentActivity fa;
     String TAG = "AddBeer";
@@ -29,11 +32,11 @@ public class AddBeerFragment extends Fragment {
         // TODO Make this not return 0.0
         origin = rl.getHeight();
         Toast.makeText(getActivity(), "RL Height is " + origin, Toast.LENGTH_SHORT).show();
+
+        // TODO Iterate!
         testButtons[0] = (Button) rl.findViewById(R.id.test_button1);
-        testButtons[0].setOnClickListener(new mClickListener());
         testButtons[0].setOnTouchListener(new mTouchListener());
         testButtons[1] = (Button) rl.findViewById(R.id.test_button2);
-        testButtons[1].setOnClickListener(new mClickListener());
         testButtons[1].setOnTouchListener(new mTouchListener());
 
         rl.setOnTouchListener(new mTouchListener());
@@ -41,37 +44,52 @@ public class AddBeerFragment extends Fragment {
         return rl;
     }
 
-    class mClickListener implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(getActivity(), "Click Beer", Toast.LENGTH_SHORT).show();
-        }
-    }
-
     class mTouchListener implements View.OnTouchListener {
+
+        // TODO Dynamasize
         float testOriginX[] = new float[2];
         float testOriginY[] = new float[2];
-        float touchOriginX, touchOriginY = 0;
+
+        float touchOriginX, touchOriginY, dY, dX = 0;
+
+        // TODO Add to a config file...
+        float clickThreshold = 5;
 
         @Override
         public boolean onTouch(View v, MotionEvent event) {
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                // TODO Iterate!
                 testOriginX[0] = testButtons[0].getX();
                 testOriginY[0] = testButtons[0].getY();
                 testOriginX[1] = testButtons[1].getX();
                 testOriginY[1] = testButtons[1].getY();
                 touchOriginX = event.getRawX();
                 touchOriginY = event.getRawY();
+                return true;
             }
+
             if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                float dY = touchOriginY - event.getRawY();
-                float dX = touchOriginX - event.getRawX();
+                dY = touchOriginY - event.getRawY();
+                dX = touchOriginX - event.getRawX();
+
+                // TODO Iterate!
                 testButtons[0].setY(testOriginY[0] - dY);
                 testButtons[0].setX(testOriginX[0] - dX);
                 testButtons[1].setY(testOriginY[1] - dY);
                 testButtons[1].setX(testOriginX[1] - dX);
+                return true;
+            }
 
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (dX < clickThreshold && dY < clickThreshold &&   // Handle Click Events
+                        dX > -clickThreshold && dY > -clickThreshold &&
+                        v.getId() != R.id.add_beer_layout) {
+
+
+                    Toast.makeText(getActivity(), "Didn't move " + dY, Toast.LENGTH_SHORT).show();
+                }
+                return true;
             }
 
             return false;
