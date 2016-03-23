57b041dad048999b85327b661625605a73fa1ad7
Mike <mthomsonbrown@gmail.com>
2016-03-22 20:48:26 -0700

    Forgot to add new beer view fragment

---
+++ app/src/main/java/com/slashandhyphen/tasterly/AddBeerFragmentAlpha.java
@@ -0,0 +1,32 @@
+package com.slashandhyphen.tasterly;
+
+import android.app.Fragment;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+
+
+/**
+ * Created by ookamijin on 3/21/2016.
+ * Development class to support all functionality
+ */
+public class AddBeerFragmentAlpha extends Fragment {
+
+    RelativeLayout rl;
+    Resources res;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        res = getResources();
+        rl = (RelativeLayout) inflater.inflate(R.layout.fragment_add_beer_alpha, container, false);
+
+
+
+        return rl;
+    }
+}
