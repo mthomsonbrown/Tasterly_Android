6613d767d15f30484e51edd80c10d2f6eb757b2e
Mike <mthomsonbrown@gmail.com>
2016-03-29 19:57:19 -0700

    Cleaned up the view xml a bit and added a comment field

--- app/src/main/java/com/slashandhyphen/tasterly/Camera.java
+++ app/src/main/java/com/slashandhyphen/tasterly/Camera.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
@@ -13,10 +14,14 @@ import java.io.File;
  * Created by ookamijin on 3/28/2016.
  *
  * This class encapsulates the interaction with the Android camera service.
+ *
+ *  TODO
+ *  Upgrade to use Camera2 api, and save the image to the database rather than a loose file.
  */
 public class Camera {
     Activity activity;
     String photoPath;
+    String TAG = "++Camera++";
 
     Camera(Activity activity) {
         this.activity = activity;
@@ -26,12 +31,19 @@ public class Camera {
         // build name. located in bin folder on sd, i believe
         String imageFileName = "Picture_" + System.currentTimeMillis()
                 + "_";
-        File image = new File(activity.getExternalFilesDir(null), imageFileName);
+        File image = new File(activity.getFilesDir(), imageFileName);
 
         photoPath = image.getAbsolutePath();
         return image;
     }
 
+    /**
+     * This is used to call the camera application and create a photo.
+     *
+     * TODO
+     * It should probably take a string as an arg to label the file, and should return a
+     * file location
+     */
     void takePicture() {
         if (!activity.getPackageManager()
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
@@ -46,7 +58,7 @@ public class Camera {
                     .putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
             int cameraReturn = 0;
             activity.startActivityForResult(takePictureIntent, cameraReturn);
-
+            Log.d(TAG, "File is: " + f.getAbsolutePath());
         }
     }
 }
