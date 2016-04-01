24fe714e5eaa264258e210271c8c6cefd21b6d96
Mike <mthomsonbrown@gmail.com>
2016-03-31 18:58:25 -0700

    Added stub fragment to show the flavor wheel.
    At this point, lifecycle events are pretty broken (i.e. pressing the back button in the flavor wheel fragment takes you back to the previous activity rather than the previous fragment.)  data is not being saved between instances, and there's probably a lot of subtle issues as well.
    
     With this configuration, it would be a good time to add some unit tests for lifecycle transitions in order to cement the interaction between the pieces.

--- app/src/main/java/com/slashandhyphen/tasterly/AddBeerAlphaFragment.java
+++ app/src/main/java/com/slashandhyphen/tasterly/AddBeerAlphaFragment.java
@@ -1,34 +1,30 @@
 package com.slashandhyphen.tasterly;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.res.Resources;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
-
 
 /**
  * Created by ookamijin on 3/21/2016.
- * Development class to support all functionality
+ * Development class to support all add beer functionality
  */
 public class AddBeerAlphaFragment extends Fragment implements View.OnClickListener  {
 
     RelativeLayout rl;
     Resources res;
     ImageButton imageButtonPhoto;
-    String photoPath;
+    Button buttonFlavorWheel;
+
     String TAG = "++Alpha Add Beer Fragment++";
     Camera camera;
 
@@ -39,6 +35,8 @@ public class AddBeerAlphaFragment extends Fragment implements View.OnClickListen
         rl = (RelativeLayout) inflater.inflate(R.layout.fragment_add_beer_alpha, container, false);
         imageButtonPhoto = (ImageButton) rl.findViewById(R.id.image_button_photo);
         imageButtonPhoto.setOnClickListener(this);
+        buttonFlavorWheel = (Button) rl.findViewById(R.id.button_flavor_wheel);
+        buttonFlavorWheel.setOnClickListener(this);
         camera = new Camera(getActivity());
 
         return rl;
@@ -49,6 +47,14 @@ public class AddBeerAlphaFragment extends Fragment implements View.OnClickListen
         switch (v.getId()) {
             case R.id.image_button_photo:
                 camera.takePicture();
+                break;
+            case R.id.button_flavor_wheel:
+                FlavorWheelAlphaFragment wheelFrag = new FlavorWheelAlphaFragment();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.add_beer_fragment_container, wheelFrag);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                ft.addToBackStack(null);
+                ft.commit();
                 break;
             default:
                 break;
